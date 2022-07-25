2ddfcf60029fbe65a7d51bfa2ae62052c81d8a28
benjamin perrin <perrin.benjamin77@gmail.com>
2022-07-25 16:52:17 +0200

    corrige ex7 part3

--- src/main/java/fr/m2i/javawebapp/PrimeNumberListServlet.java
+++ src/main/java/fr/m2i/javawebapp/PrimeNumberListServlet.java
@@ -3,41 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 package fr.m2i.javawebapp;
-
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author ben
- */
 public class PrimeNumberListServlet extends HttpServlet {
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    public static String checkPrime(int n) {
-        int count = 0;
-
-        
-        if (n < 2)
-            return n+" nombre non premier";
-
-        for (int i = 1; i <= n; i++) 
-        { 
-            if (n % i == 0) 
-                count += 1; 
-        } 
-
-        if (count > 2){
-            return n+" nombre non premier";
-
-        }else{
-            return n+" nombre premier";  
-        }  }
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -49,17 +22,6 @@ public class PrimeNumberListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            ArrayList<String> numbers = new ArrayList<String>();
-
-        if(request.getParameter("start") != null && request.getParameter("end") != null){
-            int start = Integer.parseInt(request.getParameter("start"));            
-            int end = Integer.parseInt(request.getParameter("end"));
-               for (int i = start; i <= end; i++) 
-                { 
-                    numbers.add(checkPrime(i));
-                } 
-            request.setAttribute("numbers",numbers);
-        }  
         this.getServletContext().getRequestDispatcher("/primeNumberListServlet.jsp").forward(request, response);
     }
 
@@ -74,7 +36,7 @@ public class PrimeNumberListServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            super.doGet(request, response);
+        this.getServletContext().getRequestDispatcher("/primeNumber.jsp").forward(request, response);
     }
 
     /**
--- src/main/webapp/primeNumberListServlet.jsp
+++ src/main/webapp/primeNumberListServlet.jsp
@@ -1,27 +1,55 @@
-<%-- 
-    Document   : pairs
-    Created on : 25 juil. 2022, 11:03:00
-    Author     : ben
---%>
-
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
-    <body>             
-      <form method="GET" action="PrimeNumberListServlet">
-         <label>debut: </label><input type = "text" name = "start">
-         <br />
-         <label>fin: </label><input type = "text" name = "end">
-         <br />
-         <input type = "submit" value = "Submit" />
-      </form>
-        <c:if test="${numbers != null}">
-            <c:forEach items="${numbers}" var="item" varStatus="status">
-                ${item}<br/>
-            </c:forEach>
-        </c:if>
-      
+    <jsp:include page="include/header.jsp">
+        <jsp:param name="title" value="Formulaire nombre premier" />
+    </jsp:include>
+    <body>
+        <h1>Prime Number</h1>
 
+        <form method="POST">
+            <label for="start">Début :</label>
+            <input type="number" name="start" />
+
+            <br />
+
+            <label for="end">Fin :</label>
+            <input type="number" name="end" />
+
+            <br />
+
+            <input type="submit" value="Valider" />
+        </form>
+
+        <c:set var="start" value="${param.start}" />
+        <c:set var="end" value="${param.end}" />
+
+        <c:if test="${start != null && end != null}">
+
+            <c:forEach var="current" begin="${start}" end="${end}">
+
+                <c:set var="flag" value="0" />
+                <c:forEach var="i" begin="2" end="${current / 2}">
+
+                    <c:if test="${current % i == 0 && current != i}">
+                        <c:set var="flag" value="1" />
+                    </c:if>
+
+                </c:forEach>
+
+                <c:if test="${flag == 0}">
+                    <c:out value="${current}" /> nombre premier
+                    <br />
+                </c:if>
+
+                <c:if test="${flag != 0}">
+                    <c:out value="${current}" /> nombre non premier
+                    <br />
+                </c:if>
+
+            </c:forEach>
+
+        </c:if>
     </body>
 </html>
