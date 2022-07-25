9a75a97f68f00a5ef3109db1ad019416966deb7c
benjamin perrin <perrin.benjamin77@gmail.com>
2022-07-25 10:01:04 +0200

    ex04 getHeaderNames

---
+++ src/main/java/fr/m2i/javawebapp/ShowRequestHeaders.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package fr.m2i.javawebapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ben
+ */
+public class ShowRequestHeaders extends HttpServlet {
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            //super.doGet(request, response);
+            Enumeration<String> headersName = request.getHeaderNames();
+            List<String> headerNameList = new ArrayList();
+            List<String> headerValueList = new ArrayList();
+    while(headersName.hasMoreElements()){
+        String name = headersName.nextElement();
+        headerNameList.add(name);      
+        headerValueList.add(request.getHeader(name));
+
+    }
+    // On passe par des attributs pour les utiliser côté JSP
+        request.setAttribute("headersName", headerNameList);
+        request.setAttribute("headersValue", headerValueList);
+        this.getServletContext().getRequestDispatcher("/showRequestHeaders.jsp").forward(request, response);
+
+
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            super.doGet(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/webapp/showRequestHeaders.jsp
@@ -0,0 +1,33 @@
+<%-- 
+    Document   : showRequestHeaders
+    Created on : 25 juil. 2022, 09:19:46
+    Author     : ben
+--%>
+
+<%@page import="java.util.List"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+     <jsp:include page="/include/header.jsp">
+	<jsp:param name="title" value="headers"/>
+    </jsp:include>
+    <body>
+        <h1>Hello World!</h1>
+        <table>
+            <tr>
+                <th>Nom </th>
+                <th>Valeur </th>
+            </tr>
+            <% 
+                List<String> headersName = (List) request.getAttribute("headersName");                
+                List<String> headersValue = (List) request.getAttribute("headersValue");
+                for (int i = 0; i < headersName.size(); i++) {
+                    out.println("<tr><td>" + headersName.get(i) + "</td>");
+                    out.println("<td>" + headersValue.get(i) + "</td></tr>");
+                }
+
+            %>
+        </table>
+
+    </body>
+</html>
